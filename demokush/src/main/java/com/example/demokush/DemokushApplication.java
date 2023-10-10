4acbe54ad87462f9f9f4a3747aa183a368a77ee9
Khush Mahajan <82937743+khush1998@users.noreply.github.com>
2023-10-10 14:20:34 +0530

    Add files via upload

---
+++ demokush/src/main/java/com/example/demokush/DemokushApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemokushApplication.class, args);
+	}
+
+}
---
+++ demokush/src/test/java/com/example/demokush/DemokushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demokush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
